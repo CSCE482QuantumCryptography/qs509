431295fdcd843f932d27ee94cb54e283fccfb2f5
Josef <--josefmun9902@gmail.com>
2024-03-22 12:46:09 -0500

    private key and certificate generation

--- keys.go
+++ keys.go
@@ -5,21 +5,19 @@ import (
 	"os/exec"
 )
 
-func GenerateKey(spec string) {
-	
-	cmd := exec.Command("../build/bin/openssl", "genpkey", "-algorithm", "dilithium3", "-out", "dilithium3_srv.key")
+func GenerateKey() string{
+	cmd := exec.Command("../build/bin/openssl", "req", "-x509", "-new", "-newkey", "dilithium3", "-keyout", "dilithium3_CA.key", "-out", "dilithium3_CA.crt", "-nodes", "-subj", "/CN=test CA", "-days", "365", "-config", "openssl/apps/openssl.cnf")
 
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
+		fmt.Println("error")
         fmt.Println(err.Error())
-        return
+        return string(output)
     }
 
     // Print the output
     fmt.Println(string(output))
 
-	return
-
-
+	return string(output)
 }
