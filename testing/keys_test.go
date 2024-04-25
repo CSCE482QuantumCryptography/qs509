5866bfedd733fdffe579295993e6818d416b6f1e
aheff20 <aidan.m.heffron@gmail.com>
2024-04-25 11:41:07 -0500

    updated key tests

--- testing/keys_test.go
+++ testing/keys_test.go
@@ -16,34 +16,31 @@ func Test_GenerateKey_Dilithium3(t *testing.T) {
 	var d3_sa qs509.SignatureAlgorithm
 	d3_sa.Set("DILITHIUM3")
 
-	success, err := qs509.GenerateKey(d3_sa, "dilithium3_CA.key")
+	success, err := qs509.GenerateKey(d3_sa, "dilithium3_test.key")
 	assert.NoError(t, err, "should not have an error")
 	assert.True(t, success, "should be successful")
 
 	// Clean up generated key file
-	os.Remove("dilithium3_CA.key")
+	os.Remove("dilithium3_test.key")
 }
 
-func Test_GenerateKey_LocalSignedCert(t *testing.T) {
+func Test_GenerateKey_RSA(t *testing.T) {
 	var rsa_sa qs509.SignatureAlgorithm
 	rsa_sa.Set("RSA")
 
-	success, err := qs509.GenerateKey(rsa_sa, "local_signed_cert.key")
+	success, err := qs509.GenerateKey(rsa_sa, "rsa_test.key")
 	assert.NoError(t, err, "should not have an error")
 	assert.True(t, success, "should be successful")
 
 	// Clean up generated key file
-	os.Remove("local_signed_cert.key")
+	os.Remove("rsa_test.key")
 }
 
-func Test_GenerateKey_Rainy(t *testing.T) {
+func Test_GenerateKey_Invalid(t *testing.T) {
 	var ab_sa qs509.SignatureAlgorithm
-	ab_sa.Set("AB")
+	ab_sa.Set("INVLAID")
 
-	success, err := qs509.GenerateKey(ab_sa, "unsigned_cert.key")
-	assert.NoError(t, err, "should have an error")
-	assert.True(t, success, "should not be successful")
-
-	// Clean up generated key file
-	os.Remove("unsigned_cert.key")
+	success, err := qs509.GenerateKey(ab_sa, "invalid_test.key")
+	assert.Error(t, err, "should have an error")
+	assert.False(t, success, "should not be successful")
 }
